dae0bf467b7023a0260d1b65ba41155e54b142f4
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-01 11:34:07 +0430

    Add left-side navbar with emojis

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,15 @@ export default function Home() {
         <meta name="description" content="Gmail clone" />
         <link rel="icon" href="/favicon.webp" />
       </Head>
-
+      <div id={styles.nav}>
+        <button className={styles.compose}>✍<span className={styles.span} id={styles.compose}>Compose</span></button>
+        <button className={styles.items}>📥<span className={styles.span}>Inbox</span></button>
+        <button className={styles.items}>⭐<span className={styles.span}>Starred</span></button>
+        <button className={styles.items}>😴<span className={styles.span}>Snoozed</span></button>
+        <button className={styles.items}>🚀<span className={styles.span}>Sent</span></button>
+        <button className={styles.items}>💌<span className={styles.span}>Drafts</span></button>
+        <button className={styles.items}>🔽<span className={styles.span}>More</span></button>
+      </div>
     </div>
   )
 }
