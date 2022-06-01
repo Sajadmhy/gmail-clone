62970fe35b568e754b075724ee2516dc0454df3b
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-01 15:13:14 +0430

    Add header elements with style

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,34 @@ export default function Home() {
         <meta name="description" content="Gmail clone" />
         <link rel="icon" href="/favicon.webp" />
       </Head>
+      <div id={styles.header}>
+        <button className={styles.headerItem}>📰</button>
+        <button className={styles.headerItem2}>
+          <span className={styles.img}>
+          <Image
+          src="/favicon.webp"
+          width={40}
+          height={28}
+          alt="gmail icon"
+          />
+          </span>
+          <p className={styles.gmail}>Gmail</p>
+        </button>
+        <button className={styles.searchIcon}>🔍</button>
+        <input className={styles.searchBar} type="text" placeholder='Search mail'/>
+        <button className={styles.searchSetting}>⚙</button>
+        <button className={styles.headerItem}>✋</button>
+        <button className={styles.headerItem}>👩‍🔧</button>
+        <button className={styles.headerItem}>🎮</button>
+        <button className={styles.headerProfile}>
+          <Image
+          src="/profile1.png"
+          width={50}
+          height={50}
+          alt="profile picture"
+          />
+        </button>
+      </div>
       <div id={styles.nav}>
         <button className={styles.compose}>✍<span className={styles.span} id={styles.compose}>Compose</span></button>
         <button className={styles.items}>📥<span className={styles.span}>Inbox</span></button>
