592e58ad039121340d7b56d041b4efeb3b7da2bc
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-02 18:10:50 +0430

    Add tool tip text to header icons

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Header.module.css';
 export default function Header(props) {
     return(
         <div id={styles.header}>
-        <button className={styles.headerItem} onClick={() => props.setShowNav(s => !s)}>📰</button>
+        <button className={styles.headerItem} onClick={() => props.setShowNav(s => !s)}>📰<span className={styles.tooltiptext}>Menu</span></button>
         <button className={styles.headerItem2}>
           <span className={styles.img}>
           <Image
@@ -19,9 +19,9 @@ export default function Header(props) {
         <button className={styles.searchIcon}>🔍</button>
         <input className={styles.searchBar} type="text" placeholder='Search mail'/>
         <button className={styles.searchSetting}>⚙</button>
-        <button className={styles.headerItem}>✋</button>
-        <button className={styles.headerItem}>👩‍🔧</button>
-        <button className={styles.headerItem}>🎮</button>
+        <button className={styles.headerItem}><span className={styles.tooltiptext}>Support</span>✋</button>
+        <button className={styles.headerItem}>👩‍🔧<span className={styles.tooltiptext}>Settings</span></button>
+        <button className={styles.headerItem}>🎮<span className={styles.tooltiptext}>Google Apps</span></button>
         <button className={styles.headerProfile}>
           <Image
           src="/profile1.png"
